b076e296520320d5624ccfb388e6170b7a88c9f0
Shweta94Me <mestryshweta30@gmail.com>
2020-06-26 22:38:40 -0400

    Disabled background when menu opens

--- js/script.js
+++ js/script.js
@@ -2,6 +2,8 @@ $(document).ready(function(){
     $('[data-toggle="tooltip"]').tooltip();   
     $('.menu-item').click(function(){
         $('#menu-open').prop('checked', false);
+        $('body').css('overflow-y','visible');
+        $('.sm-container').css('opacity','1');
     });
     $(window).scroll(function () {
         if ($(window).scrollTop() >= 100) {
@@ -10,6 +12,30 @@ $(document).ready(function(){
         $('.menu').css('opacity','1');
         }
         });
+    $("#menu-open").click(function(e, parameters) {
+
+        var nonUI = false;
+        try {
+            nonUI = parameters.nonUI;
+        } catch (e) {}
+        var checked = nonUI ? !this.checked : this.checked;
+        if(checked){
+            $('.menu').css('opacity','1');
+            $('body').css('overflow-y','hidden');
+            $('.sm-container').css('opacity','0.2');
+        }
+        else{
+            $('body').css('overflow-y','visible');
+            if ($(window).scrollTop() >= 100) {
+                $('.menu').css('opacity','0.7');
+                } else {
+                $('.menu').css('opacity','1');
+                }
+            // $('.menu').css('opacity','0.7');
+            $('.sm-container').css('opacity','1');
+            // $('body').css('back','rgba(0,0,0,0.4)');
+        }
+    });
     // $('.menu').hover(function(){
     //     $('.menu').css('opacity','1');
     // })
